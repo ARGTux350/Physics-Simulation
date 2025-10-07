c96d67063417109b2755333bf9c6fbf9be793721
ARGTux350 <RoqueGoulartA@stu.blsd.ca>
2025-10-07 09:50:04 -0500

    Update ProjectilePlotter.java

--- Physics Simulation/src/ProjectilePlotter.java
+++ Physics Simulation/src/ProjectilePlotter.java
@@ -1,49 +1,63 @@
 import java.io.IOException;
+import java.util.Arrays;
 import org.knowm.xchart.*;
 import org.knowm.xchart.BitmapEncoder.BitmapFormat;
+import org.knowm.xchart.style.markers.SeriesMarkers;
 import org.apache.commons.math3.fitting.PolynomialCurveFitter;
 import org.apache.commons.math3.fitting.WeightedObservedPoints;
 
-public class Main {
+public class ProjectilePlotter {
 
-    public static void main(String[] args) throws IOException {
-    	// Step 1: Input 5 known points for projectile motion
-    	double[] xInput = {0.0, 1.0, 2.0, 3.0, 4.0};
-    	double[] yInput = {2.0, 4.5, 5.0, 3.5, 1.0};
-
-
-        // Step 2: Fit a quadratic curve (degree 2)
+    public static void plot(double[] xInput, double[] yInput, String chartTitle) throws IOException {
+        // Step 1: Fit a quadratic curve (degree 2)
         WeightedObservedPoints obs = new WeightedObservedPoints();
         for (int i = 0; i < xInput.length; i++) {
             obs.add(xInput[i], yInput[i]);
         }
 
         PolynomialCurveFitter fitter = PolynomialCurveFitter.create(2); // quadratic
-        double[] coeffs = fitter.fit(obs.toList()); // returns [c, b, a] for ax^2 + bx + c
+        double[] coeffs = fitter.fit(obs.toList()); // [c, b, a] for ax^2 + bx + c
 
-        // Step 3: Generate smooth curve
+        // Step 2: Generate smooth curve
         int numPoints = 100;
+        double minX = Arrays.stream(xInput).min().getAsDouble();
+        double maxX = Arrays.stream(xInput).max().getAsDouble();
+
         double[] xData = new double[numPoints];
         double[] yData = new double[numPoints];
         for (int i = 0; i < numPoints; i++) {
-            xData[i] = -2.0 + 4.0 * i / (numPoints - 1);
+            xData[i] = minX + (maxX - minX) * i / (numPoints - 1);
             yData[i] = coeffs[2] * xData[i] * xData[i] + coeffs[1] * xData[i] + coeffs[0];
         }
 
-        // Step 4: Plot the curve
-        XYChart chart = QuickChart.getChart("Fitted Curve", "X", "Y", "y(x)", xData, yData);
-        chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
-        chart.getStyler().setMarkerSize(0);
-        chart.setXAxisTitle("Input (x)");
-        chart.setYAxisTitle("Output (y)");
+        // Step 3: Create chart
+        XYChart chart = new XYChartBuilder()
+                .width(800)
+                .height(600)
+                .title(chartTitle)
+                .xAxisTitle("Displacement (x)")
+                .yAxisTitle("Displacement (y)")
+                .build();
 
+        // Fitted curve
+        XYSeries fittedSeries = chart.addSeries("Trajectory", xData, yData);
+        fittedSeries.setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
+        fittedSeries.setLineColor(java.awt.Color.BLUE);
+        fittedSeries.setMarker(SeriesMarkers.NONE);
+
+        // Original data points
+        XYSeries dataSeries = chart.addSeries("Original Data", xInput, yInput);
+        dataSeries.setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Scatter);
+        dataSeries.setMarker(SeriesMarkers.CIRCLE);
+        dataSeries.setMarkerColor(java.awt.Color.RED);
+
+        // Style
         chart.getStyler().setAxisTitleFont(new java.awt.Font("SansSerif", java.awt.Font.BOLD, 14));
-        chart.getStyler().setAxisTitlePadding(10);
-        chart.getStyler().setXAxisLabelRotation(0);
-        chart.getStyler().setXAxisDecimalPattern("0.0");
+        chart.getStyler().setLegendVisible(true);
+        chart.getStyler().setMarkerSize(6);
 
-
-        new SwingWrapper(chart).displayChart();
+        // Show and Save
+        new SwingWrapper<>(chart).displayChart();
         BitmapEncoder.saveBitmap(chart, "./Fitted_Curve", BitmapFormat.PNG);
         BitmapEncoder.saveBitmapWithDPI(chart, "./Fitted_Curve_300_DPI", BitmapFormat.PNG, 300);
     }
