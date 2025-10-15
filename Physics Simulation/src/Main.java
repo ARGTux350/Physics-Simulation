c78e4a79131221f808df1dd84e937c4ee5361146
Nvoth8 <noahjvoth@gmail.com>
2025-10-15 09:06:06 -0500

    GUI commit

--- Physics Simulation/src/Main.java
+++ Physics Simulation/src/Main.java
@@ -1,20 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		/*
-		there are 5 different options:
-		1: find time
-			to find time, we could use equation number 3 and convert it to: t = (V - Vo) / a
-		2: find starting velocity
-			to find starting velocity, we could use equation number 3 and convert it to: Vo = V - (a * t) 
-		3: find distance
-			to find distance, we could use equation number 5: d = Vo * t + ((a / 2) * t ^ 2)
-		4: find ending velocity
-			to find ending velocity, we could use equation number 3: V = Vo + at
-		5: find acceleration
-			to find acceleration, we could use equation number 3: a = (V - Vo) / t 
-		*/
+		GUI projectileMotion = new GUI();
+		
 	}
-
 }
